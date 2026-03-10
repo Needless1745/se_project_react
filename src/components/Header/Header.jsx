a11daec4b99129627462031bb3c090a7a9d1ff4e
Needleless1745 <josemsanchez8@gmail.com>
2026-03-10 00:02:26 -0500

    Toggle switch created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -11,17 +12,22 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="WTWR logo" />
-      <p className="header__date-and-location">{currentDate}</p>
-      <button
-        onClick={handleAddClick}
-        type="button"
-        className="header__add-clothes-btn"
-      >
-        + Add clothes
-      </button>
-      <div className="header__user-container">
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      <p className="header__date-and-location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <div className="header__right">
+        <ToggleSwitch />
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add clothes
+        </button>
+        <div className="header__user-container">
+          <p className="header__username">Terrence Tegegne</p>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
       </div>
     </header>
   );
